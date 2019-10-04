d2a5e3209008eba5ab73f58d384201775c54e5cb
matthewgeale <matt@incentloyalty.com>
2019-10-04 16:25:33 +1000

    updated tsconfig, installed prettier

--- src/get_stats.ts
+++ src/get_stats.ts
@@ -1,11 +1,11 @@
 import { WSClient } from '@0x/mesh-rpc-client';
 
 (async () => {
-    const c = new WSClient("ws://localhost:60557");
+    const c = new WSClient('ws://localhost:60557');
 
     const stats = await c.getStatsAsync();
 
     console.log(JSON.stringify(stats, null, '\t'));
 
     process.exit(0);
-})()
+})();
