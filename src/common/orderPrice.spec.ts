6d67ff449082afe0a6fde2e11435c4905eb78a89
mgeale <matthewgeale@me.com>
2019-11-22 10:37:23 +1100

    re-wrote slippage and orderPrice functions

--- src/common/orderPrice.spec.ts
+++ src/common/orderPrice.spec.ts
@@ -1,12 +1,47 @@
-import { loadTestOrderInfo } from '../test/loadTestOrderInfo';
+import { BigNumber, SignedOrder } from '@0x/mesh-rpc-client';
 
-import { getOrdersPerMarket, MarketOrders } from './marketTotals';
+import { getAskOrderPrice, getBidOrderPrice } from './orderPrice';
 
-xdescribe('order price', () => {
-    let marketOrders: MarketOrders[];
+describe('order price', () => {
+    it('should calculate ask order price', () => {
+        const order: SignedOrder = {
+            makerAddress: 'maker_address',
+            makerAssetData: 'maker_asset_data',
+            makerAssetAmount: new BigNumber(272246488340000000000),
+            makerFee: new BigNumber(0),
+            takerAddress: 'taker_address',
+            takerAssetData: 'taker_asset_data',
+            takerAssetAmount: new BigNumber(1333681097079992000),
+            takerFee: new BigNumber(0),
+            senderAddress: 'sender_address',
+            exchangeAddress: 'exchange_address',
+            feeRecipientAddress: 'fee_recipient_address',
+            expirationTimeSeconds: new BigNumber(0),
+            salt: new BigNumber(0),
+            signature: 'signature'
+        };
+        const result = getAskOrderPrice([order]);
+        expect(result[0].price).toEqual(new BigNumber(0.0048988));
+    });
 
-    beforeAll(() => {
-        const orders = loadTestOrderInfo();
-        marketOrders = getOrdersPerMarket(orders);
+    it('should calculate bid order price', () => {
+        const order: SignedOrder = {
+            makerAddress: 'maker_address',
+            makerAssetData: 'maker_asset_data',
+            makerAssetAmount: new BigNumber(135090000000000000),
+            makerFee: new BigNumber(0),
+            takerAddress: 'taker_address',
+            takerAssetData: 'taker_asset_data',
+            takerAssetAmount: new BigNumber(150000000000000000000),
+            takerFee: new BigNumber(0),
+            senderAddress: 'sender_address',
+            exchangeAddress: 'exchange_address',
+            feeRecipientAddress: 'fee_recipient_address',
+            expirationTimeSeconds: new BigNumber(0),
+            salt: new BigNumber(0),
+            signature: 'signature'
+        };
+        const result = getBidOrderPrice([order]);
+        expect(result[0].price).toEqual(new BigNumber(0.0009006));
     });
 });
--- src/common/slippage.ts
+++ src/common/slippage.ts
@@ -3,37 +3,64 @@ import { BigNumber } from '@0x/mesh-rpc-client';
 import { OrderPrice } from './orderPrice';
 
 export interface Slippage {
-    prices: BigNumber[];
-    slippage: BigNumber
-    averageCost: BigNumber;
+    slippage: BigNumber;
     actualCost: BigNumber;
-    count: number;
+    filledOrders: FilledOrder[];
+}
+
+export interface FilledOrder {
+    price: BigNumber;
+    amount: BigNumber;
 }
 
 // work in progress
 export function calculateSlippage(orders: OrderPrice[], purchaseAmount: number): Slippage {
-    const prices = [];
+    const filledOrders = getFilledOrders(orders, purchaseAmount);
+
+    const actualCost = calculateTotalCostOfFilledOrders(filledOrders);
+    const averagePrice = calculateAveragePriceOfFilledOrders(orders);
+
+    const totalCostOfAveragePrice = averagePrice.multipliedBy(purchaseAmount);
+    const totalCostOfBestPrice = filledOrders[0].price.multipliedBy(purchaseAmount);
+
+    const slippage = totalCostOfBestPrice.minus(totalCostOfAveragePrice);
+    return {
+        slippage,
+        actualCost,
+        filledOrders
+    };
+}
+
+export function getFilledOrders(o: OrderPrice[], purchaseAmount: number): FilledOrder[] {
+    const orders = o.sort((a, b) =>
+        a.price > b.price ? 1 : b.price > a.price ? -1 : 0
+    );
+    const filledOrders: FilledOrder[] = [];
     let count = new BigNumber(0);
-    let actualCost = new BigNumber(0);
     for (let i = 0; count.isLessThan(purchaseAmount); i++) {
-        if (orders[i].makerAssetAmount.plus(count).isGreaterThan(purchaseAmount)) {
+        if (orders[i].amount.plus(count).isGreaterThan(purchaseAmount)) {
             const remaining = new BigNumber(purchaseAmount).minus(count);
-            prices.push(orders[i].price);
+            filledOrders.push({ price: orders[i].price, amount: remaining });
             count = count.plus(remaining);
-            actualCost = orders[i].price.multipliedBy(remaining).plus(actualCost);
         } else {
-            prices.push(orders[i].price);
-            count = orders[i].makerAssetAmount.plus(count);
-            actualCost = orders[i].price.multipliedBy(orders[i].makerAssetAmount).plus(actualCost);
+            filledOrders.push({ price: orders[i].price, amount: orders[i].amount });
+            count = count.plus(orders[i].amount);
         }
     }
-    const averageCost = actualCost.dividedBy(count);
-    const slippage = prices[0].minus(averageCost);
-    return {
-        prices,
-        slippage,
-        averageCost,
-        actualCost,
-        count: count.toNumber()
-    };
+    return filledOrders;
+}
+
+export function calculateAveragePriceOfFilledOrders(filledOrders: FilledOrder[]): BigNumber {
+    const prices = filledOrders.map(o => o.price);
+    const sum = prices.reduce((total, number) => {
+        return number.plus(total);
+    }, new BigNumber(0));
+    return sum.dividedBy(prices.length);
+}
+
+export function calculateTotalCostOfFilledOrders(filledOrders: FilledOrder[]): BigNumber {
+    return filledOrders.reduce((total, number) => {
+        const orderAmount = number.amount.multipliedBy(number.price);
+        return orderAmount.plus(total);
+    }, new BigNumber(0));
 }
