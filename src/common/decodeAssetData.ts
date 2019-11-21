147ccdd5181b7b4594381895b68459569287369f
mgeale <matthewgeale@me.com>
2019-11-21 18:13:34 +1100

    improvements to decodeAssetData

--- src/common/decodeAssetData.ts
+++ src/common/decodeAssetData.ts
@@ -3,22 +3,18 @@ import { assetDataUtils } from '@0x/order-utils';
 
 export interface DecodeAssetData {
     assetProxyId: string;
-    tokenAddress: string;
+    tokenAddress?: string;
     amounts?: BigNumber[];
     tokenId?: BigNumber;
 }
 
 export function decodeAssetData(encodedAssetData: string): DecodeAssetData {
-    const erc20RegEx = new RegExp('^0xf47261b0');
-    const erc721RegEx = new RegExp('^0x02571792');
-    const multiAsset = new RegExp('^0x94cfcdd7');
+    const decodedAssetData = assetDataUtils.decodeAssetDataOrThrow(encodedAssetData)
 
-    if (erc20RegEx.test(encodedAssetData)) {
-        return assetDataUtils.decodeERC20AssetData(encodedAssetData);
-    } else if (erc721RegEx.test(encodedAssetData)) {
-        return assetDataUtils.decodeERC721AssetData(encodedAssetData);
-    } else if (multiAsset.test(encodedAssetData)) {
+    if (assetDataUtils.isMultiAssetData(decodedAssetData)) {
         return decodeMultiAssetData(encodedAssetData);
+    } else {
+        return decodedAssetData;
     }
 }
 
